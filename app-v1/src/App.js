f0ffd360c53002fdd92189310ba0e2d48db7dd90
Jeremy Odell <jeremyodell@jeremys-mbp.wireless.newschool.edu>
2022-04-25 17:16:43 -0400

    BASIC INIT SCROLLY

--- app-v1/src/App.js
+++ app-v1/src/App.js
@@ -1,6 +1,7 @@
 import { useState, useEffect, useRef } from 'react'
 import cloneDeep from "lodash/cloneDeep"
 import * as d3 from 'd3'
+import { Scrollama, Step } from 'react-scrollama'
 
 import useWindowSize from './hooks/useWindowSize';
 
@@ -19,6 +20,7 @@ import * as C from './logic/constants'
 import { stops } from './logic/data'
 
 import transit from './assets/images/transit.png'
+import curve from './assets/images/curve.png'
 
 import './App.css';
 
@@ -34,6 +36,8 @@ function App() {
   const [peopleTotal, setPeopleTotal] = useState([])
   // orchestration
   const [isMoving, setIsMoving] = useState(false)
+  // scrolly
+  const [currentStepIndex, setCurrentStepIndex] = useState(null);
   // specific charts
   const [genderStack, setGenderStack] = useState([])
   const [raceStack, setRaceStack] = useState([])
@@ -128,6 +132,8 @@ function App() {
 
     train.transition().duration(1500)
       .style('height', '50vh')
+
+    map.style('overflow-y', 'hidden')
   }
 
   const moveFirstStep = () => {
@@ -157,6 +163,12 @@ function App() {
     moveMiddleSteps
   ]
 
+  // scrolly
+  const onStepEnter = ({ data }) => {
+    console.log('entered steop', data)
+    setCurrentStepIndex(data);
+  };
+
   return (
     <div id="app">
       <div
@@ -175,11 +187,33 @@ function App() {
         {/* <div style={{ fontFamily: "'Helvetica'", height: '100vh', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
           <img style={{ height: '100%' }} src={transit} />
         </div> */}
-        <div style={{ fontFamily: "'Helvetica'", height: '100vh', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
-          {windowSize.height && <Map
-            height={'100%'}
-            width={windowSize.width}
-          />}
+        <div style={{ fontFamily: "'Helvetica'", backgroundColor: 'darkgrey', height: 900* 12, display: 'flex', flexDirection: 'column', alignItems: 'end', justifyContent: 'center' }}>
+          <div style={{ position: 'sticky', height: '100vh', width: '100vw', top: 0 }}>
+            <img style={{ height: '100%' }} src={curve} />
+          </div>
+          <Scrollama offset={0.5} onStepEnter={onStepEnter}>
+            {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map((_, stepIndex) => (
+              <Step data={stepIndex} key={stepIndex}>
+                <div
+                  style={{
+                    height: '900px',
+                    // border: '1px solid gray',
+                    display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
+                    zIndex: 10
+                    // opacity: currentStepIndex === stepIndex ? 1 : 0.2,
+                  }}
+                >
+                  <div style={{ height: 200, width: 200, border: '1px solid grey', backgroundColor: 'black', boxShadow: '10px 5px 5px red', marginRight: 50 }}>
+                    <p>This is text about the map</p>
+                    <p>The map will change on scroll</p>
+                    <p>This is my favorite map ever. map map map</p>
+                  </div>
+                </div>
+              </Step>
+            ))}
+          </Scrollama>
+          
+
         </div>
         {windowSize.height && <MapChart
           innerRef={mapRef}
